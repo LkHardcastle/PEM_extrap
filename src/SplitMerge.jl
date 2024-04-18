3f5691b8768783edd9c0ba7167c2edec194f7fae
LkHardcastle <hardcastle.luke@gmail.com>
2024-04-18 17:19:02 +0100

    Fixed many bugs - seems to be working but needs fine tuning

--- src/SplitMerge.jl
+++ src/SplitMerge.jl
@@ -1,22 +1,33 @@
 function split_int!(x::Matrix{Float64}, v::Matrix{Float64}, s::Matrix{Bool}, t::Float64, Q_f::PriorityQueue, Q_s::PriorityQueue, Q_m::PriorityQueue, dat::PEMData, j::CartesianIndex, priors::Prior, dyn::Dynamics)
     s[j] = true
     delete!(Q_s, j)
-    v[j] = 2*rand(Bernoulli(0.5)) - 1.0
+    v[j] = (2*rand(Bernoulli(0.5)) - 1.0)*dyn.v_abs[j]
     x[j] = 0.0
     new_bound!(Q_f, t, x, v, s, priors, dat, dyn, j, true)
+    for l in (j[2] + 1):size(x0,2)
+        if s[l]
+            new_bound!(Q_f, t, x, v, s, priors, dat, dyn, CartesianIndex(j[1],l), false)
+        end
+    end
     new_merge!(Q_m, t, x, v, s, j, true)
 end
 
 function merge_int!(x::Matrix{Float64}, v::Matrix{Float64}, s::Matrix{Bool}, t::Float64, Q_f::PriorityQueue, Q_s::PriorityQueue, Q_m::PriorityQueue, dat::PEMData, j::CartesianIndex, priors::Prior, dyn::Dynamics)
     s[j] = false
     v[j] = 0.0
-    new_split!(Q_s, s, t, priors, j)
+    x[j] = 0.0
+    new_split!(Q_s, s, t, priors, j, dyn)
+    for l in (j[2] + 1):size(x0,2)
+        if s[l]
+            new_bound!(Q_f, t, x, v, s, priors, dat, dyn, CartesianIndex(j[1],l), false)
+        end
+    end
     delete!(Q_m, j)
     delete!(Q_f, j)
 end
 
-function new_split!(Q_s::PriorityQueue, s::Matrix{Bool}, t::Float64, priors::Prior, j::CartesianIndex)
-    enqueue!(Q_s, j, t + rand(Exponential(1/split_rate(s, dat, priors, j))))
+function new_split!(Q_s::PriorityQueue, s::Matrix{Bool}, t::Float64, priors::Prior, j::CartesianIndex, dyn::Dynamics)
+    enqueue!(Q_s, j, t + rand(Exponential(1/split_rate(s, dat, priors, j, dyn))))
 end
 
 function new_merge!(Q_m::PriorityQueue, t::Float64, x::Matrix{Float64}, v::Matrix{Float64}, s::Matrix{Bool}, j::CartesianIndex, new::Bool)
@@ -36,9 +47,9 @@ function merge_time(x::Matrix{Float64}, v::Matrix{Float64}, j::CartesianIndex)
     end
 end
 
-function split_rate(s::Matrix{Bool}, dat::PEMData, priors::Prior, j::CartesianIndex)
+function split_rate(s::Matrix{Bool}, dat::PEMData, priors::Prior, j::CartesianIndex, dyn::Dynamics)
     #'return 0.375*abs(sum(dot([1,-2],[1,-1])))*priors.p_split*(priors.ω[j]/(1 - priors.ω[j]))*(sqrt(2*pi*priors.σ^2))^-1
-    return priors.p_split*(priors.ω[j]/(1 - priors.ω[j]))*(sqrt(2*pi*priors.σ^2))^-1
+    return abs(dyn.v_abs[j])*priors.p_split*(priors.ω[j]/(1 - priors.ω[j]))*(sqrt(2*pi*priors.σ^2))^-1
 end
 
 function v_size(s::Matrix{Bool}, j::CartesianIndex)
--- test/main tests.jl
+++ test/main tests.jl
@@ -9,38 +9,34 @@ include(srcdir("PreProcessing.jl"))
 include(srcdir("PostProcessing.jl"))
 
 Random.seed!(123)
-n = 0
-y = rand(Exponential(0.5),n)
-#maximum(y)
+n = 100
+y = rand(Exponential(1.0),n)
+maximum(y)
 #breaks = collect(0.05:0.05:(maximum(y) + 0.1))
 #breaks = collect(0.05:0.05:1.0)
-breaks = [1.0,2.0]
+breaks = collect(1:6)
 p = 1
 cens = fill(1.0,n)
 covar = fill(1.0, 1, n)
 dat = init_data(y, cens, covar, breaks)
 x0, v0, s0 = init_params(p, dat)
-v0 = ones(size(v0))
+v0[2] = 0.1
+v0[3] = 0.2
+v0[4] = 0.3
+v0[5] = 0.4
+v0[6] = 0.5
 t0 = 0.0
 priors = FixedPrior(fill(0.5, size(x0)), 1.0, 1.0, 0.0, 1.0)
-nits = 100_000
-nsmp = 10_000
-settings = Settings(nits, nsmp, 0.9, 0.5, 0.0, false)
+nits = 1_000_000
+nsmp = 100_000
+settings = Settings(nits, nsmp, 0.9, 0.5, 0.0, v0, false)
 Random.seed!(23653)
 out1 = @time pem_sample(x0, s0, v0, t0, dat, priors, settings)
 
 Random.seed!(3546232)
-x0, v0, s0 = init_params(p, dat)
-v0 = ones(size(v0))
-#v0[2] = 2.0
-priors = FixedPrior(fill(0.5, size(x0)), 0.5, 1.0, 0.0, 1.0)
 out2 = @time pem_sample(x0, s0, v0, t0, dat, priors, settings)
 
 Random.seed!(2222)
-x0, v0, s0 = init_params(p, dat)
-v0 = ones(size(v0))
-priors = FixedPrior(fill(0.5, size(x0)), 2.0, 1.0, 0.0, 1.0)
-v0[2] = 2.0
 out3 = @time pem_sample(x0, s0, v0, t0, dat, priors, settings)
 
 smps1 = out1["Smp_x"]
@@ -51,62 +47,48 @@ mean(smps1[:,2] .== 0.0)
 mean(smps2[:,2] .== 0.0)
 mean(smps3[:,2] .== 0.0)
 
-mean(smps1[5_000:end,1] .== smps1[5_000:end,2])
-mean(smps2[5_000:end,1] .== smps2[5_000:end,2])
-mean(smps3[5_000:end,1] .== smps3[5_000:end,2])
-
-v1 = out1["Smp_v"]
-histogram(v1[:,1])
-histogram(v1[:,2])
-
-histogram(vec(out1["Sk_x"][:,1,1:10_000]))
-histogram(vec(out1["Sk_x"][:,2,1:10_000]))
-
 mean(smps1[:,1])
 mean(smps2[:,1])
 mean(smps3[:,1])
-quantile(smps1[:,1],0.025)
-quantile(smps2[:,1],0.025)
-quantile(smps3[:,1],0.025)
-quantile(Normal(0,1),0.025)
-quantile(smps1[:,1],0.975)
-quantile(smps2[:,1],0.975)
-quantile(smps3[:,1],0.975)
-quantile(Normal(0,2),0.975)
 
 mean(smps1[:,2])
 mean(smps2[:,2])
 mean(smps3[:,2])
 
-mean(smps1[findall(smps1[:,1] .== smps1[:,2]),1])
-mean(smps2[findall(smps2[:,1] .== smps2[:,2]),1])
-mean(smps3[findall(smps3[:,1] .== smps3[:,2]),1])
-quantile(smps1[findall(smps1[:,1] .== smps1[:,2]),1],0.025)
-quantile(smps2[findall(smps2[:,1] .== smps2[:,2]),1],0.025)
-quantile(smps3[findall(smps3[:,1] .== smps3[:,2]),1],0.025)
-quantile(smps1[findall(smps1[:,1] .== smps1[:,2]),1],0.975)
-quantile(smps2[findall(smps2[:,1] .== smps2[:,2]),1],0.975)
-quantile(smps3[findall(smps3[:,1] .== smps3[:,2]),1],0.975)
+mean(smps1[:,2] .== 0.0)
+mean(smps2[:,2] .== 0.0)
+mean(smps3[:,2] .== 0.0)
 
+mean(smps1[:,3])
+mean(smps2[:,3])
+mean(smps3[:,3])
 
-mean(smps1[findall(smps1[:,1] .!= smps1[:,2]),1])
-mean(smps2[findall(smps2[:,1] .!= smps2[:,2]),1])
-mean(smps3[findall(smps3[:,1] .!= smps3[:,2]),1])
-quantile(smps1[findall(smps1[:,1] .!= smps1[:,2]),1],0.025)
-quantile(smps2[findall(smps2[:,1] .!= smps2[:,2]),1],0.025)
-quantile(smps3[findall(smps3[:,1] .!= smps3[:,2]),1],0.025)
-quantile(smps1[findall(smps1[:,1] .!= smps1[:,2]),1],0.975)
-quantile(smps2[findall(smps2[:,1] .!= smps2[:,2]),1],0.975)
-quantile(smps3[findall(smps3[:,1] .!= smps3[:,2]),1],0.975)
+mean(smps1[:,3] .== 0.0)
+mean(smps2[:,3] .== 0.0)
+mean(smps3[:,3] .== 0.0)
+
+quantile(smps1[:,1],0.025)
+quantile(smps2[:,1],0.025)
+quantile(smps3[:,1],0.025)
+
+quantile(smps1[:,1],0.975)
+quantile(smps2[:,1],0.975)
+quantile(smps3[:,1],0.975)
+
+mean(smps1[:,2])
+mean(smps2[:,2])
+mean(smps3[:,2])
+
 
 
 mean(smps1[findall(0.0 .!= smps1[:,2]),2])
-mean(smps2[findall(0.0 .!= smps1[:,2]),2])
-mean(smps3[findall(0.0 .!= smps1[:,2]),2])
+mean(smps2[findall(0.0 .!= smps2[:,2]),2])
+mean(smps3[findall(0.0 .!= smps3[:,2]),2])
 quantile(Normal(0,sqrt(1.25)),0.025)
 quantile(smps1[findall(0.0 .!= smps1[:,2]),2],0.025)
+quantile(smps1[findall(0.0 .!= smps1[:,2]),2],0.975)
 quantile(Normal(0,sqrt(1 + 5^2)),0.025)
-quantile(smps2[findall(0.0 .!= smps1[:,2]),2],0.025)
+quantile(smps2[findall(0.0 .!= smps2[:,2]),2],0.025)
 quantile(Normal(0,sqrt(2)),0.025)
 quantile(smps3[findall(0.0 .!= smps3[:,2]),2],0.025)
 quantile(smps1[findall(0.0 .!= smps1[:,2]),2],0.975)
@@ -145,7 +127,7 @@ plot(collect(1:size(smps1[:,1],1)),cumsum(smps1[:,1] .== smps1[:,2])./collect(1:
 plot!(collect(1:size(smps2[:,1],1)),cumsum(smps2[:,1] .== smps2[:,2])./(1:size(smps2[:,1],1)))
 plot!(collect(1:size(smps3[:,1],1)),cumsum(smps3[:,1] .== smps3[:,2])./(1:size(smps3[:,1],1)))
 hline!([0.5,0.5])
-n_plot = 5100
+n_plot = 10_000
 n_start = 5000
 plot(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot])
 plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
@@ -173,15 +155,21 @@ plot(scatter(quantile(Normal(0,1), collect(0.000002:0.000002:0.999998)),sort(smp
 plot(scatter(quantile(Normal(0,2), collect(0.00001:0.00001:0.99999)),sort(smps2[2:end,1])))
 plot(scatter(quantile(Normal(0,2), collect(0.00001:0.00001:0.99999)),sort(smps3[2:end,1])))
 
-n_plot = 20
-n_start = 1
+n_plot = 10_000
+n_start = 5000
 plot(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot])
 plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
-plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
+plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,3,:])[n_start:n_plot])
+
+plot(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot])
+plot(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
+plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,3,:])[n_start:n_plot])
 
 
-plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot])
-plot!(out1["t"][n_start:n_plot], vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
-plot(out2["t"][n_start:n_plot], vec(out2["Sk_x"][:,1,:])[n_start:n_plot] .- vec(out2["Sk_x"][:,2,:])[n_start:n_plot])
-plot!(out2["t"][n_start:n_plot], vec(out2["Sk_x"][:,1,:])[n_start:n_plot])
-plot!(out2["t"][n_start:n_plot], vec(out2["Sk_x"][:,2,:])[n_start:n_plot])
+plot(vec(out1["Sk_x"][:,1,:])[n_start:n_plot], vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
+plot(vec(out1["Sk_x"][:,2,:])[n_start:n_plot], vec(out1["Sk_x"][:,3,:])[n_start:n_plot])
+
+
+plot(vec(out1["Sk_x"][:,1,:])[n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot])
+plot(vec(out1["Sk_x"][:,1,:])[n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,3,:])[n_start:n_plot])
+plot(vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot], vec(out1["Sk_x"][:,1,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,2,:])[n_start:n_plot] .+ vec(out1["Sk_x"][:,3,:])[n_start:n_plot])
