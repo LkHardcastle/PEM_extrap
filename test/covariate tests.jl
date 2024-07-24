9f32a687c5be2cf11e0cb58da1da47091187e70b
lkhardcastle <hardcastle.luke@gmail.com>
2024-07-24 10:11:25 +0100

    bump

---
+++ test/covariate tests.jl
@@ -0,0 +1,32 @@
+using DrWatson
+@quickactivate "PEM_extrap"
+# For src
+using DataStructures, LinearAlgebra, Distributions, Random, Optim, Roots, SpecialFunctions
+
+include(srcdir("Sampler.jl"))
+include(srcdir("PreProcessing.jl"))
+include(srcdir("PostProcessing.jl"))
+
+Random.seed!(12515)
+n = 1
+y = rand(Exponential(1.0),n)
+breaks = collect(0.5:0.5:5)
+p = 1
+cens = fill(1.0,n)
+covar = fill(1.0, 1, n)
+dat = init_data(y, cens, covar, breaks)
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+
+Random.seed!(34734)
+dat = init_data(y, cens, covar, breaks)
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+state0 = ECMC2(x0, v0, s0, t0, true, findall(s0))
+nits = 10_000
+nsmp = 100000
+settings = Settings(nits, nsmp, 1_000_000, 0.5,0.0, 0.1, false, true)
+println(state0.x);println(state0.v)
+priors = BasicPrior(1.0, FixedV(1.0), FixedW(0.5), 0.0)
+@time out3 = pem_sample(state0, dat, priors, settings)
