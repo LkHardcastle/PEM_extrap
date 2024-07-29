c3b3cfc88e58fcf4dc2b17f9eb2d694fe0efa06e
lkhardcastle <hardcastle.luke@gmail.com>
2024-07-29 11:24:35 +0100

    Cts grid

--- src/HyperUpdates.jl
+++ src/HyperUpdates.jl
@@ -1,6 +1,71 @@
-function hyper_update!(state::State, priors::Prior)
+function hyper_update!(state::State, dyn::Dynamics, dat::PEMData, priors::Prior)
     variance_update!(state, priors, priors.σ)
     weight_update!(state, priors, priors.ω)
+    grid_update!(state, dyn, dat, priors, priors.grid)
+end
+
+function grid_update!(state::State, dyn::Dynamics, dat::PEMData, priors::Prior, Grid::Fixed)
+end
+
+function grid_update!(state::State, dyn::Dynamics, dat::PEMData, priors::Prior, grid::Cts)
+    rem_ind = findall(sum(eachcol(state.s)) .!= 0.0)
+    state.x, state.v, state.s, state.g, state.s_loc = state.x[:,rem_ind], state.v[:,rem_ind], state.s[:,rem_ind], state.g[:,rem_ind], state.s_loc[rem_ind]
+    J_curr = cumsum(state.s, dims = 2)
+    J_new = min(rand(Poisson(grid.max_time*grid.Γ*(1 - priors.ω.ω))), grid.max_points - J_curr)
+    J_row = rand(DiscreteUniform(1:size(state.x,1)), J_new)
+    J_loc = rand(Uniform(0,grid.max_time), J_new)
+    g_new = fill(false, size(state.x, 1),J_loc)
+    for i in 1:J_new
+        g_new[J_row[i], i] = true
+    end
+    ind_new = sortperm(vcat(state.s_loc, J_loc))
+    zero_mat = zeros(size(state.x, 1),J_loc)
+    state.s_loc = vcat(state.s_loc, J_loc)[ind_new]
+    state.x = hcat(state.x, zero_mat)[ind_new]
+    state.v = hcat(state.v, zero_mat)[ind_new]
+    state.s = hcat(state.s, fill(false, size(zero_mat)))[ind_new]
+    state.g = hcat(state.g, g_new)[ind_new]
+    state.active = findall(state.s)
+    dat_update!(state, dyn, dat)
+end
+
+function dat_update!(state::State, dyn::Dynamics, dat::PEMData)
+    L = size(dat.UQ, 2)
+    J = size(breaks,1)
+    if size(dat.UQ, 2) == dat.n
+        grp = collect(1:dat.n)
+    else
+        grp = zeros(dat.n)
+        for i in axes(dat.covar,2)
+            for j in axes(dat.UQ,2)
+                if UQ[:,j] == covar[:,i]
+                    grp[i] = j
+                end
+            end
+        end
+    end
+    W = zeros(L,J)
+    δ = zeros(L,J)
+    d = zeros(Int, length(dat.y))
+    for i in eachindex(dat.y)
+        d[i] = findfirst(state.s_loc .> dat.y[i])
+    end
+    for l in 1:L
+        yl = y[findall(grp .== l)]
+        dl = d[findall(grp .== l)]
+        δl = cens[findall(grp .== l)]
+        for j in 1:J
+            if j == 1
+                sj1 = 0.0
+            else
+                sj1 = state.s_loc[j-1]
+            end
+            W[l,j] = sum(yl[findall(dl .== j)] .- sj1) + length(findall(dl .> j))*(state.s_loc[j] - sj1)
+            δ[l,j] = length(intersect(findall(δl .== 1), findall(dl .== j)))
+        end
+    end
+    dyn.W = W
+    dyn.δ = δ
 end
 
 function variance_update!(state::State, priors::Prior, σ::FixedV)
--- src/Types.jl
+++ src/Types.jl
@@ -4,6 +4,8 @@ mutable struct BPS <: State
     x::Matrix{Float64}
     v::Matrix{Float64}
     s::Matrix{Bool}
+    g::Matrix{Bool}
+    s_loc::Vector{Float64}
     t::Float64
     active::Array{CartesianIndex{2}}
 end
@@ -12,6 +14,8 @@ mutable struct ECMC <: State
     x::Matrix{Float64}
     v::Matrix{Float64}
     s::Matrix{Bool}
+    g::Matrix{Bool}
+    s_loc::Vector{Float64}
     t::Float64
     active::Array{CartesianIndex{2}}
 end
@@ -20,6 +24,8 @@ mutable struct ECMC2 <: State
     x::Matrix{Float64}
     v::Matrix{Float64}
     s::Matrix{Bool}
+    g::Matrix{Bool}
+    s_loc::Vector{Float64}
     t::Float64
     b::Bool
     active::Array{CartesianIndex{2}}
@@ -39,6 +45,8 @@ mutable struct Dynamics
     A::Matrix{Float64}
     V::Matrix{Float64}
     S::Matrix{Bool}
+    δ::Matrix{Int64}
+    W::Matrix{Float64}
     sampler_eval::SamplerEval
 end
 
@@ -55,11 +63,13 @@ mutable struct Storage
     x::Array{Float64}
     v::Array{Float64}
     s::Array{Bool}
+    s_loc::Array{Float64}
     t::Vector{Float64}
     h::Array{Float64}
     x_smp::Array{Float64}
     v_smp::Array{Float64}
     s_smp::Array{Bool}
+    s_loc_smp::Array{Float64}
     t_smp::Vector{Float64}
     h_smp::Array{Float64}
 end
@@ -90,6 +100,17 @@ mutable struct Beta <: Weight
     b::Float64
 end
 
+abstract type Grid end
+
+mutable struct Fixed <: Grid
+end
+
+mutable struct Cts <: Grid
+    Γ::Float64
+    max_points::Int64
+    max_time::Float64
+end
+
 abstract type Prior end
 
 #mutable struct BasicPrior <: Prior
@@ -104,6 +125,7 @@ mutable struct BasicPrior <: Prior
     σ::Variance
     ω::Weight
     p_split::Float64
+    grid::Grid
 end
 
 mutable struct ARPrior <: Prior
