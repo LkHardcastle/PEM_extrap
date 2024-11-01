7035195b6b20f5f59872414fc42c8063ec73ace9
lkhardcastle <hardcastle.luke@gmail.com>
2024-11-01 16:02:33 +0000

    RJ exp finished

--- scripts/RJ experiment.jl
+++ scripts/RJ experiment.jl
@@ -132,11 +132,37 @@ priors1 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 1.0, Cts(5.0, 100.0, 3.2
 priors2 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.2, 100.0, 3.2), [RandomWalk()])
 priors3 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.2, 100.0, 3.2), [RandomWalk()])
 
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+nits = 100_000
+nsmp = 500_000
+state0 = ECMC2(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0))
+settings = Settings(nits, nsmp, 10_000_000, 1.0, 1.0, 1.0, false, true)
+out = pem_sample(state0, dat, priors1, settings)
+mean(cts_transform(cumsum(out["Smp_x"], dims = 2), out["Smp_s_loc"], grid), dims = 3)[1,:,1]
+mean(sum(out["Smp_s"],dims = 2)[1,1,:])
+
+priors3 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.1, 100.0, 3.2), [RandomWalk()])
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+nits = 5_000_000
+nsmp = 10_000_000
+state0 = RWM(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0), 0.05, 0)
+settings = Settings(nits, nsmp, 1_000_000, 1.0, 1.0, 1.0, false, true)
+out1 = pem_sample(state0, dat, priors3, settings)
+mean(out1["Sk_J"])
+mean(cts_transform(cumsum(out1["Smp_x"], dims = 2), out1["Smp_s_loc"], grid), dims = 3)[1,:,1]
+
+
 sampler = []
 J_vec = []
 h_mat = Matrix{Float64}(undef, 3, 0)
 it = []
 tuning = []
+nits = 10_000
+nsmp = 50_000
 for i in 1:exp_its
     x0, v0, s0 = init_params(p, dat)
     v0 = v0./norm(v0)
@@ -200,8 +226,8 @@ p = 1
 cens = df.status
 covar = fill(1.0, 1, n)
 dat = init_data(y, cens, covar, breaks)
-nits = 50_000
-nsmp = 50_000
+nits = 100_000
+nsmp = 500_000
 
 Random.seed!(14124)
 grid = [0.5, 1.5, 2.5]
@@ -209,6 +235,30 @@ exp_its = 1
 priors1 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 1.0, Cts(5.0, 100.0, 3.2), [RandomWalk()])
 priors2 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.2, 100.0, 3.2), [RandomWalk()])
 priors3 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.2, 100.0, 3.2), [RandomWalk()])
+
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+nits = 500_000
+nsmp = 1_000_000
+state0 = ECMC2(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0))
+settings = Settings(nits, nsmp, 10_000_000, 1.0, 1.0, 1.0, false, true)
+out = pem_sample(state0, dat, priors1, settings)
+mean(cts_transform(cumsum(out["Smp_x"], dims = 2), out["Smp_s_loc"], grid), dims = 3)[1,:,1]
+mean(sum(out["Smp_s"],dims = 2)[1,1,:])
+
+priors3 = BasicPrior(0.2, FixedV([0.2]), FixedW([0.5]), 0.0, RJ(5.0, 0.1, 100.0, 3.2), [RandomWalk()])
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+nits = 5_000_000
+nsmp = 5_000_000
+state0 = RWM(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0), 0.05, 0)
+settings = Settings(nits, nsmp, 1_000_000, 1.0, 1.0, 1.0, false, true)
+out1 = pem_sample(state0, dat, priors3, settings)
+mean(out1["Sk_J"])
+mean(cts_transform(cumsum(out1["Smp_x"], dims = 2), out1["Smp_s_loc"], grid), dims = 3)[1,:,1]
+
 
 sampler = []
 J_vec = []
@@ -277,6 +327,41 @@ mean(out3["Sk_J"])
 df1 = CSV.read(datadir("RJExp1.csv"), DataFrame)
 df2 = CSV.read(datadir("RJExp2.csv"), DataFrame)
 df3 = CSV.read(datadir("RJExp3.csv"), DataFrame)
+df1[!,"Exp"] .= "Prior"
+df2[!,"Exp"] .= "Changepoint"
+df3[!,"Exp"] .= "Colon data" 
+
+df = vcat(df1,df2,df3)
+
+R"""
+dat = data.frame($df)
+dat = dat %>%
+    pivot_longer(J:h3, names_to = "Param", values_to = "Mean_est") %>%
+    mutate(Exp = factor(Exp, c("Prior", "Changepoint", "Colon data")))
+param_names <- c(
+    `J` = "J",
+    `h1` = "h(0.5)",
+    `h2` = "h(1.5)",
+    `h3` = "h(2.5)"
+    )
+dat %>%
+    ggplot(aes(x = as.factor(Tuning), y = Mean_est, fill = Sampler)) + geom_boxplot() +
+    theme_classic() + facet_wrap(Param ~ Exp, scales = "free", nrow = 4, labeller = labeller(Param = param_names)) +
+    theme(axis.text.x = element_text(angle = 45, hjust=1), legend.position = "bottom") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h1"), aes(yintercept = -0.41), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h2"), aes(yintercept = -0.2), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h3"), aes(yintercept = -0.1), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "J"), aes(yintercept = 8.3), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h1"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h2"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h3"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "J"), aes(yintercept = 8.5), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h1"), aes(yintercept = -1.2), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h2"), aes(yintercept = -1.57), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h3"), aes(yintercept = -1.96), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "J"), aes(yintercept = 10.8), linetype = "dashed") + ylab("Estimate") + xlab("Tuning parameter")
+    ggsave($plotsdir("RJ_exp.pdf"), width = 8, height = 10.5)
+"""
 
 R"""
 dat1 = data.frame($df1)
