7039f2b3ed6e29e7fdc7d8feff5afef33fb8b7dc
lkhardcastle <hardcastle.luke@gmail.com>
2025-02-21 21:20:14 +0000

    RJ exp setup

--- scripts/EulerMaruyama.jl
+++ scripts/EulerMaruyama.jl
@@ -17,81 +17,6 @@ library(coda)
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 """
 
-Random.seed!(34124)
-df = CSV.read(datadir("colon.csv"), DataFrame)
-y = df.years
-maximum(y)
-n = length(y)
-breaks = collect(0.1:0.1:3.1)
-p = 1
-cens = df.status
-covar = fill(1.0, 1, n)
-dat = init_data(y, cens, covar, breaks)
-x0, v0, s0 = init_params(p, dat)
-v0 = v0./norm(v0)
-t0 = 0.0
-state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
-nits = 5_000
-nsmp = 5_000
-settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 50.0)
-
-
-priors1 = BasicPrior(1.0, PC([1.0], [2], [0.5], Inf), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 150.0, 3.2), [GaussLangevin(2.0,0.2)], [0.1])
-priors2 = EulerMaruyama(1.0, PC([1.0], [2], [0.5], Inf), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 150.0, 3.2), [GaussLangevin(2.0,0.2)], [0.1])
-
-Random.seed!(3463)
-test_times = collect(0.1:0.5:3.0)
-@time out1 = pem_fit(state0, dat, priors1, settings, test_times)
-Random.seed!(24562)
-@time out2 = pem_fit(state0, dat, priors2, settings, test_times)
-
-plot(out1[3])
-plot!(out2[3])
-plot(out1[4])
-plot!(out2[4])
-
-Random.seed!(1237)
-grid = sort(unique(out1[1]["Sk_s_loc"][cumsum(out1[1]["Sk_s"],dims = 1)[1,:,:] .> 0.0]))
-grid = grid[1:10:length(grid)]
-breaks_extrap = collect(3.02:0.25:4.02)
-@time extrap1 = barker_extrapolation(out1[1], priors1.diff[1], priors1.grid, breaks_extrap[begin], breaks_extrap[end] + 0.1, breaks_extrap, 1)
-@time test_smp1 = cts_transform(cumsum(out1[1]["Sk_θ"], dims = 2), out1[1]["Sk_s_loc"], grid)
-s1 = vcat(view(exp.(test_smp1), 1, :, :), view(exp.(extrap1), :, :))
-df1 = DataFrame(hcat(vcat(grid, breaks_extrap), median(s1, dims = 2), quantile.(eachrow(s1), 0.025), quantile.(eachrow(s1), 0.25), quantile.(eachrow(s1), 0.75), quantile.(eachrow(s1), 0.975)), :auto)
-#plot(scatter(log.(out1[1]["Smp_σ"][1,:]),test_smp[1,10,:], alpha = 0.1))
-#histogram(test_smp[1,10,:], bins = -5:0.1:5, alpha = 0.5, normalize = true)
-#histogram!(test_smp1[1,10,:], bins = -5:0.1:5, alpha = 0.5, normalize = true)
-
-grid = sort(unique(out2[2]["Sk_s_loc"][cumsum(out2[2]["Sk_s"],dims = 1)[1,:,:] .> 0.0]))
-grid = sort(unique(out1[1]["Sk_s_loc"][cumsum(out1[1]["Sk_s"],dims = 1)[1,:,:] .> 0.0]))
-grid = grid[1:10:length(grid)]
-@time extrap2 = barker_extrapolation(out2[2], priors2.diff[1], priors2.grid, breaks_extrap[begin], breaks_extrap[end] + 0.1, breaks_extrap, 1)
-@time test_smp2 = cts_transform(cumsum(out2[2]["Sk_θ"][:,:,1:1:end], dims = 2), out2[2]["Sk_s_loc"][:,:,1:1:end], grid)
-s1 = vcat(view(exp.(test_smp2), 1, :, :), view(exp.(extrap2), :, :))
-df2 = DataFrame(hcat(vcat(grid, breaks_extrap), median(s1, dims = 2), quantile.(eachrow(s1), 0.025), quantile.(eachrow(s1), 0.25), quantile.(eachrow(s1), 0.75), quantile.(eachrow(s1), 0.975)), :auto)
-
-
-R"""
-dat1 = data.frame($df1)
-dat1 = cbind(dat1, "Barker")
-colnames(dat1) <- c("Time","Mean","LCI","Q1","Q4","UCI","Model") 
-dat2 = data.frame($df2)
-dat2 = cbind(dat2, "Euler-Maruyama")
-colnames(dat2) <- c("Time","Mean","LCI","Q1","Q4","UCI","Model")  
-dat_1 <- rbind(dat1, dat2)
-"""
-
-R"""
-p1 <- dat_1 %>%
-    subset(Time < 3.0) %>%
-    pivot_longer(c(Mean, LCI, UCI)) %>%
-    ggplot(aes(x = Time, y = value, col = Model, linetype = name)) + geom_step() +
-    theme_classic() + guides(col = guide_legend(nrow = 1), linetype = FALSE) + 
-    theme(legend.position = "bottom", text = element_text(size = 20)) + scale_colour_manual(values = cbPalette[c(6,7)]) +
-    scale_linetype_manual(values = c("dotdash","solid","dotdash")) + ylab("h(t)") + xlab("Time (years)") #+ ylim(0,0.5) + xlim(0,3) 
-    p1
-"""
-
 Random.seed!(12515)
 n = 0
 y = rand(Exponential(1.0),n)
@@ -109,6 +34,8 @@ nsmp = 5_000
 settings = Splitting(nits, nsmp, 1_000_000, 1.0, 0.0, 0.1, false, true, 0.05, 10.0)
 test_times = [10,50,90.0]
 var0 = [0.001, 0.01, 0.05, 0.1, 0.25, 0.5]
+
+Random.seed!(2482)
 Barker1 = Vector{Vector{Float64}}()
 for i in eachindex(var0)
     priors = BasicPrior(1.0, FixedV([i]), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 100.0, 3.2), [GaussLangevin(2.0,2.0)], [])
---
+++ scripts/RJ exp.jl
@@ -0,0 +1,368 @@
+using DrWatson
+@quickactivate "PEM_extrap"
+# For src
+using DataStructures, LinearAlgebra, Distributions, Random, Optim, Roots, SpecialFunctions, Statistics
+using Plots, CSV, DataFrames, RCall, Interpolations, MCMCDiagnosticTools
+
+include(srcdir("Sampler.jl"))
+include(srcdir("PreProcessing.jl"))
+include(srcdir("PostProcessing.jl"))
+
+R"""
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(cowplot)
+library(coda)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+"""
+
+
+Random.seed!(12515)
+n = 0
+y = rand(Exponential(1.0),n)
+breaks = collect(0.1:0.1:3.0)
+p = 1
+cens = fill(1.0,n)
+covar = fill(1.0, 1, n)
+dat = init_data(y, cens, covar, breaks)
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+nits = 10_000
+nsmp = 1_000
+test_times = collect(0.15:0.5:3.0)
+sampler = []
+J_vec = []
+h_mat = Matrix{Float64}(undef, 3, 0)
+it = []
+tuning = []
+exp_its = 5
+test_times = [0.5]
+h_test = [0.5, 1.5, 2.5]
+
+for i in 1:exp_its
+    priors1 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 100.0, 3.1), [RandomWalk()], [])
+    x0, v0, s0 = init_params(p, dat)
+    v0 = v0./norm(v0)
+    t0 = 0.0
+    state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+    settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+    out = pem_fit(state0, dat, priors1, settings, test_times)
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(it, 2*i - 1)
+    push!(it, 2*i)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+end
+
+println("-----------")
+
+tuning_param = [0.01,0.1,0.2,0.5,1.0]
+for σ in tuning_param
+    for i in 1:exp_its
+        priors2 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+        out = pem_fit(state0, dat, priors2, settings, test_times)
+        push!(J_vec,mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+    end
+end
+println("-----------")
+nits = 250_000
+for σ in tuning_param
+    for i in 1:exp_its
+        priors3 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = RWM(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0), 0.05, 0)
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 50.0)
+        out = pem_fit(state0, dat, priors3, settings, test_times)
+        push!(J_vec, mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+    end
+end
+
+
+df = DataFrame(Sampler = sampler, Iter = it, Tuning = tuning, J = J_vec,  h1 = h_mat[1,:], h2 = h_mat[2,:], h3 = h_mat[3,:])
+CSV.write(datadir("RJExp1.csv"), df)
+
+
+Random.seed!(12515)
+n = 200
+breaks = collect(0.1:0.1:3.1)
+p = 1
+y = zeros(n)
+cens = zeros(n)
+y1 = rand(Exponential(2.0),n)
+y2 = rand(Exponential(1.0),n)
+for i in 1:n
+    if y1[i] < 1.0
+        y[i] = y1[i]
+        cens[i] = 1.0
+    elseif y2[i] < 2.0
+        y[i] = y2[i] + 1.0
+        cens[i] = 1.0
+    else
+        y[i] = 3.0 
+        cens[i] = 0.0
+    end
+end
+p = 1
+covar = fill(1.0, 1, n)
+dat = init_data(y, cens, covar, breaks)
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+nits = 10_000
+nsmp = 1_000
+test_times = collect(0.15:0.5:3.0)
+sampler = []
+J_vec = []
+h_mat = Matrix{Float64}(undef, 3, 0)
+it = []
+tuning = []
+exp_its = 5
+test_times = [0.5]
+h_test = [0.5, 1.5, 2.5]
+
+for i in 1:exp_its
+    priors1 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 100.0, 3.1), [RandomWalk()], [])
+    x0, v0, s0 = init_params(p, dat)
+    v0 = v0./norm(v0)
+    t0 = 0.0
+    state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+    settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+    out = pem_fit(state0, dat, priors1, settings, test_times)
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(it, 2*i - 1)
+    push!(it, 2*i)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+end
+
+println("-----------")
+
+tuning_param = [0.01,0.1,0.2,0.5,1.0]
+for σ in tuning_param
+    for i in 1:exp_its
+        priors2 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+        out = pem_fit(state0, dat, priors2, settings, test_times)
+        push!(J_vec,mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+    end
+end
+println("-----------")
+nits = 250_000
+for σ in tuning_param
+    for i in 1:exp_its
+        priors3 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = RWM(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0), 0.05, 0)
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 50.0)
+        out = pem_fit(state0, dat, priors3, settings, test_times)
+        push!(J_vec, mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+    end
+end
+
+
+df = DataFrame(Sampler = sampler, Iter = it, Tuning = tuning, J = J_vec,  h1 = h_mat[1,:], h2 = h_mat[2,:], h3 = h_mat[3,:])
+CSV.write(datadir("RJExp2.csv"), df)
+
+Random.seed!(12515)
+df = CSV.read(datadir("colon.csv"), DataFrame)
+y = df.years
+maximum(y)
+n = length(y)
+breaks = collect(0.1:0.1:3.1)
+p = 1
+cens = df.status
+covar = fill(1.0, 1, n)
+dat = init_data(y, cens, covar, breaks)
+x0, v0, s0 = init_params(p, dat)
+v0 = v0./norm(v0)
+t0 = 0.0
+state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+nits = 10_000
+nsmp = 1_000
+test_times = collect(0.15:0.5:3.0)
+sampler = []
+J_vec = []
+h_mat = Matrix{Float64}(undef, 3, 0)
+it = []
+tuning = []
+exp_its = 5
+test_times = [0.5]
+h_test = [0.5, 1.5, 2.5]
+
+for i in 1:exp_its
+    priors1 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 1.0, CtsPois(10.0, 10.0, 100.0, 3.1), [RandomWalk()], [])
+    x0, v0, s0 = init_params(p, dat)
+    v0 = v0./norm(v0)
+    t0 = 0.0
+    state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+    settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+    out = pem_fit(state0, dat, priors1, settings, test_times)
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+    h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+    push!(it, 2*i - 1)
+    push!(it, 2*i)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+    push!(sampler,"PDMP")
+    push!(tuning, 0.0)
+end
+
+println("-----------")
+
+tuning_param = [0.01,0.1,0.2,0.5,1.0]
+for σ in tuning_param
+    for i in 1:exp_its
+        priors2 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = ECMC2(x0, v0, s0, collect(.!s0), breaks, t0, length(breaks), true, findall(s0))
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 25.0)
+        out = pem_fit(state0, dat, priors2, settings, test_times)
+        push!(J_vec,mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+        push!(sampler,"PDMPRJ")
+        push!(tuning, σ)
+    end
+end
+println("-----------")
+nits = 250_000
+for σ in tuning_param
+    for i in 1:exp_its
+        priors3 = BasicPrior(1.0, FixedV([0.5]), FixedW([0.5]), 0.0, RJ(10.0, 1, σ, 100.0, 3.1), [RandomWalk()], [])
+        x0, v0, s0 = init_params(p, dat)
+        v0 = v0./norm(v0)
+        t0 = 0.0
+        state0 = RWM(x0, v0, s0, fill(false, size(s0)), breaks, t0, length(breaks),  true, findall(s0), 0.05, 0)
+        settings = Splitting(nits, nsmp, 1_000_000, 1.0, 5.0, 0.1, false, true, 0.05, 50.0)
+        out = pem_fit(state0, dat, priors3, settings, test_times)
+        push!(J_vec, mean(sum(out[1]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[1]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(J_vec,mean(sum(out[2]["Sk_s"],dims = 2)[1,1,:]))
+        h_mat = hcat(h_mat, mean(cts_transform(cumsum(out[1]["Sk_x"], dims = 2), out[2]["Sk_s_loc"], h_test), dims = 3)[1,:,1])
+        push!(it, 2*i - 1)
+        push!(it, 2*i)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+        push!(sampler,"MHRJ")
+        push!(tuning, σ)
+    end
+end
+
+
+df = DataFrame(Sampler = sampler, Iter = it, Tuning = tuning, J = J_vec,  h1 = h_mat[1,:], h2 = h_mat[2,:], h3 = h_mat[3,:])
+CSV.write(datadir("RJExp3.csv"), df)
+
+df1 = CSV.read(datadir("RJExp1.csv"), DataFrame)
+df2 = CSV.read(datadir("RJExp2.csv"), DataFrame)
+df3 = CSV.read(datadir("RJExp3.csv"), DataFrame)
+df1[!,"Exp"] .= "Prior"
+df2[!,"Exp"] .= "Changepoint"
+df3[!,"Exp"] .= "Colon data" 
+df = vcat(df1,df2,df3)
+df.Tuning
+R"""
+dat = data.frame($df)
+dat = dat %>%
+    pivot_longer(J:h3, names_to = "Param", values_to = "Mean_est") %>%
+    mutate(Exp = factor(Exp, c("Prior", "Changepoint", "Colon data")))
+
+param_names <- c(
+    `J` = "J",
+    `h1` = "h(0.5)",
+    `h2` = "h(1.5)",
+    `h3` = "h(2.5)"
+    )
+
+dat %>%
+    ggplot(aes(x = as.factor(Tuning), y = Mean_est, fill = Sampler)) + geom_boxplot() +
+    theme_classic() + facet_wrap(Param ~ Exp, scales = "free", nrow = 4, labeller = labeller(Param = param_names)) +
+    theme(axis.text.x = element_text(angle = 45, hjust=1), legend.position = "bottom") 
+"""
+R"""
+    + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h1"), aes(yintercept = -0.41), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h2"), aes(yintercept = -0.2), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "h3"), aes(yintercept = -0.1), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Changepoint", Param == "J"), aes(yintercept = 8.3), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h1"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h2"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "h3"), aes(yintercept = 0.0), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Prior", Param == "J"), aes(yintercept = 8.5), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h1"), aes(yintercept = -1.2), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h2"), aes(yintercept = -1.57), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "h3"), aes(yintercept = -1.96), linetype = "dashed") + 
+    geom_hline(data = filter(dat, Exp == "Colon data", Param == "J"), aes(yintercept = 10.8), linetype = "dashed") + ylab("Estimate") + xlab("Tuning parameter")
+    #ggsave($plotsdir("RJ_exp.pdf"), width = 8, height = 10.5)
+"""
